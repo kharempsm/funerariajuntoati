a4c23440edb6066ab9d4f7ce1df609e04ec5c607
kharempsm <kharempsm@gmail.com>
2024-11-17 15:57:43 -0300

    header fijo

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,2 +1,16 @@
 const currentYear = new Date().getFullYear();
     document.getElementById("year").textContent = currentYear;
+
+    const header = document.getElementById("header");
+
+// Establecer el valor cuando el header debe volverse fijo
+const sticky = header.offsetTop; // Esto obtiene la distancia desde la parte superior del documento
+
+// Función para activar la clase 'fixed-header' cuando el scroll supere los 100 píxeles
+window.onscroll = function() {
+    if (window.pageYOffset > 100) { // Si el desplazamiento es mayor a 100 píxeles
+        header.classList.add("fixed-header");
+    } else {
+        header.classList.remove("fixed-header");
+    }
+};
